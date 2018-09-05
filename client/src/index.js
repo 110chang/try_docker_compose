6880dd48119bcb705b8f331e3a5c9f388eead452
110chang <u@110chang.com>
2018-09-05 19:06:02 +0900

    Adding socket for client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-document.onreadystatechange = function () {
+import io from 'socket.io-client';
+
+document.onreadystatechange = () => {
   if (document.readyState == 'interactive') {
     const body = document.getElementsByTagName('body')[0];
     const h2 = document.createElement('h2');
     h2.innerHTML = 'hoge';
     body.appendChild(h2);
+
+    const socket = io('http://192.168.99.100:8080');
+
+    socket.on('connect', () => {
+      console.log('connect')
+    });
   }
 };
