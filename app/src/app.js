5edf5cfb74fc50ac716d8aa79faa0277cbbdaae7
110chang <u@110chang.com>
2018-09-05 15:42:11 +0900

    First commit

---
+++ app/src/app.js
@@ -0,0 +1,16 @@
+const redis = require('redis');
+const http = require('http');
+
+const redisClient = redis.createClient(6379, 'redis');
+const PORT = 8080;
+
+
+const server = http.createServer((request, response) => {
+  redisClient.incr('counter', (error, reply) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(`You accessed here ${reply} times.\n`);
+  });
+});
+
+server.listen(PORT, '0.0.0.0');
+console.log(`Server is running on port ${PORT}.`);
